b0572b8255f15bddb84c91aa33bb1abaffe69885
Tobias Seelinger <tobias.seelinger@web.de>
2020-12-14 21:56:01 +0100

    Create connector

---
+++ src/backend/connection_tools/__init__.py
@@ -0,0 +1,11 @@
+﻿# by Tobias Seelinger
+# https://sedware.net
+
+"""
+Dieses Modul ist für die Verbindung von Server und Client
+"""
+
+# Imports
+from .connector import Connection
+
+__all__ = ['Connection']
